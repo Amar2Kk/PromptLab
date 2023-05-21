d2d7f6288d8ac7dd22c1b5af3077daa96dfd7884
Ammar <amarabas9733@gmail.com>
2023-05-21 20:14:27 +0300

    Add Web Analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Feed from '@components/Feed'
-
+import { Analytics } from '@vercel/analytics/react';
 
 const Home = () => {
   return (
@@ -10,6 +10,7 @@ const Home = () => {
       <p className="desc text-center">PromptLab is an open-source AI Prompting tool for modern world to discover, create and share creative prompts.</p>
 
       <Feed />
+      <Analytics/>
     </section>
   )
 }
